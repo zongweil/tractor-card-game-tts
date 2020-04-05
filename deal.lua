f474d488879ea98302bd72b3512d0cd43e2393c1
Zongwei Li <L.zongwei@gmail.com>
2020-04-04 17:53:14 -0700

    Format and add more comments.

--- deal.lua
+++ deal.lua
@@ -1,35 +1,42 @@
+-- Scripts for the "Deal" button. When clicked, cards from the deck will
+-- automatically be dealt to all seated players.
+
+-- The GUID of the "template" deck (for cloning purposes). If the GUID of
+-- the deck in TTS changes, this will need to be updated accordingly.
+templateDeckGUID = '1ef81b'
+
 function onload()
-  -- clickable area
-  self.createButton({
-      click_function="click", function_owner=self,
-      position={0, 0.2, 0}, height=700, width=700, color={1,1,1,0}, label=""
-  })
-  -- button label
-  self.createButton({
-      click_function="click", function_owner=self,
-      position={0, 0.2, 0}, height=1, width=1, color={1,1,1,1}, label="Deal", font_size=200
-  })
+    -- Clickable area
+    self.createButton({
+        click_function = "click", function_owner = self,
+        position = {0, 0.2, 0}, height = 700, width = 700, color = {1, 1, 1, 0}, label = ""
+    })
+    -- Button label
+    self.createButton({
+        click_function = "click", function_owner = self,
+        position = {0, 0.2, 0}, height = 1, width = 1, color = {1, 1, 1, 1}, label = "Deal", font_size = 200
+    })
 end
 
 function click(gameObj, playerColor)
-  self.AssetBundle.playTriggerEffect(0)
-  print("Dealing cards, pressed by " .. playerColor .. ".")
-  local allObjects = getAllObjects()
-  --Loop through allObjects
-  for _, object in ipairs(allObjects) do
-
-      if object.tag == 'Deck' and object.guid != '1ef81b' then
-          object.shuffle()
-          dealDeck(object, playerColor)
-      end
-  end
+    self.AssetBundle.playTriggerEffect(0)
+    printToAll("Dealing cards, pressed by " .. playerColor .. ".")
+    local allObjects = getAllObjects()
+    for _, object in ipairs(allObjects) do
+        if object.tag == 'Deck' and object.guid != templateDeckGUID then
+            object.shuffle()
+            dealDeck(object, playerColor)
+        end
+    end
 end
 
 function dealDeck(deck, playerColorWhoPressed)
-  local numSeatedPlayers = #getSeatedPlayers()
-  local numCardsToDeal = 156 / numSeatedPlayers
-  for index, currentPlayerColor in ipairs(getSeatedPlayers()) do
-    Wait.time(function() deck.deal(1, currentPlayerColor) end, 2, numCardsToDeal)
-  end
-  Wait.time(function() deck.deal(6, playerColorWhoPressed) end, (2 * numCardsToDeal) + 2)
+    local numSeatedPlayers = #getSeatedPlayers()
+    local numCardsToDeal = 100 / numSeatedPlayers
+    -- Deal 100 of the 108 cards, 1 to each player at a time.
+    for index, currentPlayerColor in ipairs(getSeatedPlayers()) do
+        Wait.time(function() deck.deal(1, currentPlayerColor) end, 2, numCardsToDeal)
+    end
+    -- Deal the remaining 8 cards to the player that pressed the button.
+    Wait.time(function() deck.deal(8, playerColorWhoPressed) end, (2 * numCardsToDeal) + 2)
 end
--- reset.lua
+++ reset.lua
@@ -1,29 +1,33 @@
+-- Scripts for the "Reset" button. When clicked, all cards on the table should
+-- be cleared, and a new deck should be placed on the table.
+
+-- The GUID of the "template" deck (for cloning purposes). If the GUID of 
+-- the deck in TTS changes, this will need to be updated accordingly.
+templateDeckGUID = '1ef81b'
+
 function onload()
-  -- clickable area
-  self.createButton({
-      click_function="click", function_owner=self,
-      position={0, 0.2, 0}, height=700, width=700, color={1,1,1,0}, label=""
-  })
-  -- button label
-  self.createButton({
-      click_function="click", function_owner=self,
-      position={0, 0.2, 0}, height=1, width=1, color={1,1,1,1}, label="Reset", font_size=200
-  })
+    -- Clickable area
+    self.createButton({
+        click_function = "click", function_owner = self,
+        position = {0, 0.2, 0}, height = 700, width = 700, color = {1, 1, 1, 0}, label = ""
+    })
+    -- Button label
+    self.createButton({
+        click_function = "click", function_owner = self,
+        position = {0, 0.2, 0}, height = 1, width = 1, color = {1, 1, 1, 1}, label = "Reset", font_size = 200
+    })
 end
 
 function click(gameObj, playerColor)
-  self.AssetBundle.playTriggerEffect(0)
-  print("Clearing cards from table, pressed by " .. playerColor .. ".")
-  local allObjects = getAllObjects()
-  --Loop through allObjects
-  for _, object in ipairs(allObjects) do
-
-      if object.tag == "Card" or (object.tag == 'Deck' and object.guid != '1ef81b') then
-          object.destruct()
-      end
-  end
-  local deckTemplate = getObjectFromGUID('1ef81b')
-  local clone_parameters = {position = {0, 0, 0}}
-  local newDeck = deckTemplate.clone(clone_parameters)
-  newDeck.setLock(false)
+    self.AssetBundle.playTriggerEffect(0)
+    printToAll("Clearing cards from table, pressed by " .. playerColor .. ".")
+    local allObjects = getAllObjects()
+    for _, object in ipairs(allObjects) do
+        if object.tag == "Card" or (object.tag == 'Deck' and object.guid != templateDeckGUID) then
+            object.destruct()
+        end
+    end
+    local cloneParameters = {position = {0, 0, 0}}
+    local newDeck = getObjectFromGUID(templateDeckGUID).clone(cloneParameters)
+    newDeck.setLock(false)
 end
